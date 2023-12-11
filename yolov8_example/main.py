bff076371f20d74cd8849d6c5245387f90010f1f
SeptAlfauzan <alfauzansepta@gmail.com>
2023-12-11 19:30:16 +0700

    add count tracking using SORT algorithm

--- yolov8_example/main.py
+++ yolov8_example/main.py
@@ -1,26 +1,64 @@
+import numpy as np
 from ultralytics import YOLO
 import cv2
 import time
 import base64
 from ultralytics.utils.plotting import Annotator
+import argparse
+
+from sort import Sort
+
+# Initialize object tracker
+tracker = Sort(max_age=100)
 
 # Inisialisasi model YOLO
 model = YOLO("yolov8n.pt")
 
 video_path = "./videos/4.mp4"
-cap = cv2.VideoCapture(video_path)
+
+parser = argparse.ArgumentParser(description="YoloX inference using OpenCV")
+parser.add_argument(
+    "--input",
+    "-i",
+    type=str,
+    help="Path to the input video. Omit for using default camera.",
+)
+parser.add_argument(
+    "--tracking",
+    "-t",
+    type=bool,
+    help="Value to display total count tracking of motorcycle",
+)
+
+parser.add_argument(
+    "--confidence",
+    "-c",
+    type=float,
+    help="Result confidence of motorcycle object that will use as threshold to show",
+)
+
+args = parser.parse_args()
+confidence = 0.5
+
+if args.input is not None:
+    cap = cv2.VideoCapture(args.input)
+else:
+    cap = cv2.VideoCapture(video_path)
+
+if args.confidence is not None:
+    confidence = args.confidence
+
 pre_timeframe = 0
 new_timeframe = 0
 
+totalCount = []
 
 while True:
     # Baca frame dari video
     ret, frame = cap.read()
-
     # Hentikan program jika video sudah habis
     if not ret:
         break
-
     new_timeframe = time.time()
     fps = 1 / (new_timeframe - pre_timeframe)
     pre_timeframe = new_timeframe
@@ -33,6 +71,8 @@ while True:
     motorcycle_count = 0
 
     for r in results:
+        detections = np.empty((0, 5))
+
         annotator = Annotator(frame)
 
         boxes = r.boxes
@@ -42,9 +82,14 @@ while True:
             ]  # dapatkan koordinat kotak dalam format (top, left, bottom, right)
             c = box.cls
 
-            print(model.names[int(c)])
+            if model.names[int(c)] == "motorcycle" and box.conf[0] > confidence:
+                score = box.conf.item() * 100
+                class_id = int(box.cls.item())
+                x1, y1, x2, y2 = np.squeeze(box.xyxy.numpy()).astype(int)
 
-            if model.names[int(c)] == "motorcycle":
+                currentArray = np.array([x1, y1, x2, y2, score])
+
+                detections = np.vstack((detections, currentArray))
                 # Tambahkan jumlah sepeda motor
                 motorcycle_count += 1
 
@@ -54,9 +99,35 @@ while True:
                     label,
                     color=(255, 0, 255),
                 )
+        resultsTracker = tracker.update(detections)
 
         frame = annotator.result()
 
+        for result in resultsTracker:
+            # Get the tracker results
+            x1, y1, x2, y2, id = result
+            x1, y1, x2, y2, id = int(x1), int(y1), int(x2), int(y2), int(id)
+            # print(result)
+
+            # Display current objects IDs
+            w, h = x2 - x1, y2 - y1
+            cx, cy = x1 + w // 2, y1 + h // 2
+            id_txt = f"ID: {str(id)}"
+
+            if id not in totalCount:
+                totalCount.append(id)
+
+            cv2.putText(
+                frame,
+                id_txt,
+                org=(cx, cy),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1.4,
+                thickness=3,
+                color=(0, 255, 255),
+            )
+
+        # print(id)
     (h, w) = frame.shape[:2]
     # tambah fps ke window
     cv2.putText(
@@ -67,16 +138,29 @@ while True:
         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
         fontScale=1,
     )
-    cv2.putText(
-        frame,
-        f"Jumlah Sepeda Motor: {motorcycle_count}",
-        (10, 30),  # Koordinat teks di dalam gambar (x, y)
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,  # Skala teks
-        (255, 0, 255),  # Warna teks (dalam format BGR)
-        2,  # Ketebalan teks
-        cv2.LINE_AA,
-    )
+
+    if args.tracking is None:
+        cv2.putText(
+            frame,
+            f"Jumlah Sepeda Motor: {motorcycle_count}",
+            (10, 30),  # Koordinat teks di dalam gambar (x, y)
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,  # Skala teks
+            (255, 0, 255),  # Warna teks (dalam format BGR)
+            2,  # Ketebalan teks
+            cv2.LINE_AA,
+        )
+    else:
+        cv2.putText(
+            frame,
+            f"Total Sepeda Motor: {len(totalCount)}",
+            (10, 30),  # Koordinat teks di dalam gambar (x, y)
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,  # Skala teks
+            (255, 0, 255),  # Warna teks (dalam format BGR)
+            2,  # Ketebalan teks
+            cv2.LINE_AA,
+        )
 
     frame = cv2.resize(frame, (960, 540))
 
